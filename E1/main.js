cb6b9f9936b94702ac9af0a54ed85ab8e132cd4a
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2025-01-09 23:51:58 +0400

    Initial commit

---
+++ E1/main.js
@@ -0,0 +1,29 @@
+let a = 100;
+let b = 150;
+let c = 12;
+let d = 5;
+let f = 3;
+
+let myName = "Vefa"
+let mySurname = "Eliyeva"
+let myAge = 31
+let myCity = "Baku"
+
+let cem = a + b;
+
+let ferq = b - a;
+
+let hasil = b * c;
+
+let nisbet = b / c;
+
+let quvvet = d ** f;
+
+console.log("İki ədədin cəmi: ", cem);
+console.log("İki ədədin fərqi: ", ferq);
+console.log("İki ədədin hasili: ", hasil);
+console.log("İki ədədin nisbəti: ", nisbet);
+console.log("İki ədədin qüvvəti: ", quvvet);
+
+let cumle = myName + " " + mySurname + " " + myAge + " " + "yaş" + " " + myCity
+console.log(cumle);
