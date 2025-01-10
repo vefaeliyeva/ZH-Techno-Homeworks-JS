6a0293eeeb51c9fd66338d11f974a5e324512d5d
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2025-01-10 10:56:14 +0400

    Completed :)

---
+++ E3/main.js
@@ -0,0 +1,38 @@
+// Q1 
+let a = 10;
+console.log("Question 1: " , a);
+// Q2
+let name = "ZH Techno School";
+console.log("Question 2: ", name);
+// Q3
+let x= 10;
+let y = 20;
+let cem = x + y
+console.log("Question 3: ", cem);
+// Q4
+let course = "ZH Techno School";
+console.log("Question 4: " , "Name of our Course: " ,course);
+// Q5
+console.log("Question 5: ", "Hello World!");
+// Q5 
+let coderPhrase = "Hello World"
+console.log("Question 5: " , coderPhrase);
+// Q6
+let number1 = 70;
+let number2 = 80;
+let sum = number1 + number2;
+console.log("Question 6: " , sum);
+//Q7
+let bolunen = 5;
+let bolen = 4;
+let qaliq = bolunen % bolen;
+console.log("Question 7: " ,qaliq);
+// Q8
+let aa = 5;
+aa+=4;
+console.log("Question 8: " , aa);
+
+
+
+
+
