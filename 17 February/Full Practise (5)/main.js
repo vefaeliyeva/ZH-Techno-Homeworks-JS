62e5b2ed787e00507529ff37f7cb8c718cb8ed91
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2025-02-18 20:15:32 +0400

    Removes vowels

--- 17 February/Full Practise (5)/main.js
+++ 17 February/Full Practise (5)/main.js
@@ -1,11 +1,14 @@
-let newPhrase ="";
- 
+
 let letters = function(str){
+    let newPhrase ="";
     for (let i = 0; i < str.length; i++) {         
-            if(str[i] != "a" && str[i] != "e" && str[i] != "u" && str[i] != "i" && str[i] != "o"){
+            if(str[i] != "a" && str[i] != "e" && str[i] != "u" && str[i] != "i" && str[i] != "o" &&
+                str[i] != "A" && str[i] != "E" && str[i] != "U" && str[i] != "I" && str[i] != "O"
+            ){
                 newPhrase +=str[i];
             }
         }
         return newPhrase;
 }
 console.log(letters("Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, alias. Sed modi ut officia"));
+console.log(letters("salam salamlar salamlarda"));
