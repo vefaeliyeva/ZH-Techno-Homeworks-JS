c757f6998f66600afb5a1e4fbe72deb53899222d
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2025-02-19 19:05:24 +0400

    Concat Method

---
+++ February-19-2025/Concat/main.js
@@ -0,0 +1,10 @@
+let students = ["Aynur", "Jale", "Aytac", "Gunay", "Hecer"]
+let teachers = ["Ceyhun", "Hakim", "Teymur", "Kenan", "Fuad",]
+
+console.log(students.concat(teachers));
+
+let vegetables = ["cucumber", "potato", "pepper", "onion", "tomato"];
+
+let newVegetables = vegetables.concat(["mushroom", "carrot"])
+
+console.log(newVegetables);
