79286b1a4e69ff4c6d6378cbe48e17eac9c0a6ac
Vəfa Əliyeva <vefa.eliyeva.ve@gmail.com>
2025-02-21 11:28:37 +0400

    Slice Method Another Example

--- February-19-2025/SliceMethod/main.js
+++ February-19-2025/SliceMethod/main.js
@@ -1,6 +1,17 @@
 let fruits = ["apple","banana","orange","pear","strawberry","blackberry","grape","mango"];
-
 let newFruits = fruits.slice(1,4)
 
 console.log(newFruits);
 console.log(fruits);
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let newNumbers = function (numbers, startIndex, endIndex) {
+    return numbers.slice(startIndex, endIndex)
+}
+
+console.log(newNumbers(numbers, 2, numbers.length-2));
+
+
+
+
+
